267e157980e68b603de7cb5f1975f62042621459
alexandre21ssouza <alexandre2121ssouza@gmail.com>
2023-08-08 17:37:30 -0300

    Primeira Estruturação do projeto

---
+++ minhaapi/src/main/java/com/alexandre/api/MinhaapiApplication.java
@@ -0,0 +1,13 @@
+package com.alexandre.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinhaapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinhaapiApplication.class, args);
+	}
+
+}
